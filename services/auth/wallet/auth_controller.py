1555575921058e6115d443d84a68b243a7d7a260
socialsuit <socialsuitai@gmail.com>
2025-06-07 14:10:24 +0500

    Fixed import path

--- services/auth/wallet/auth_controller.py
+++ services/auth/wallet/auth_controller.py
@@ -2,7 +2,7 @@ from datetime import datetime, timedelta
 from jose import jwt
 import secrets
 
-from models.user import WalletUserModel  # Assuming you're using a DB model
+from services.models.user import WalletUserModel  # Assuming you're using a DB model
 from auth.wallet.auth_schema import (
     WalletNonceRequest,
     WalletSignatureVerifyRequest,
